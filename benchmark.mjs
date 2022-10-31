7f5821c444616f9f9953c964afedcb4270da9a7e
Michael Rivnak <rivnakm1@gmail.com>
2022-10-31 10:26:58 -0400

    add benchmarking script

---
+++ benchmark.mjs
@@ -0,0 +1,49 @@
+#!/usr/bin/env zx
+
+import average from "average";
+import { stringify } from "csv-stringify/sync";
+
+$.verbose = true;
+process.env.TIMEFORMAT = "%R";
+
+async function time(cmd) {
+  let time = await $`time ${cmd} > /dev/null`;
+  return parseFloat(time.stderr.trim());
+}
+
+let iterations = argv.iterations || argv.i || 2;
+let generations = argv.generation || argv.g || 100;
+let size = argv.size || argv.s || "100x50";
+
+// Build
+
+// Run Benchmark
+const results = new Object();
+for (let i = 0; i < iterations; i++) {
+  console.log(`Running pass ${i + 1} of ${iterations}`);
+
+  if (i === 0) {
+    results["python"] = [];
+  }
+  results["python"].push(
+    await time([
+      "python",
+      "python/game_of_life.py",
+      "--iterations",
+      generations,
+      "--size",
+      size,
+    ])
+  );
+}
+
+console.log(results);
+const averages = [{ language: "language", time: "time" }];
+for (const [key, value] of Object.entries(results)) {
+  let result = average(value)
+  averages.push({ language: key, time: result });
+}
+console.log(averages);
+
+const output = stringify(averages);
+
