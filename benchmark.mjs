7705c3047d2ca052256ca7fc294c681754b46881
Michael Rivnak <rivnakm1@gmail.com>
2022-11-01 00:52:13 -0400

    pretty output

--- benchmark.mjs
+++ benchmark.mjs
@@ -7,7 +7,7 @@ import average from "average";
 import { stringify } from "csv-stringify/sync";
 import { table } from "table";
 
-$.verbose = true;
+$.verbose = false;
 process.env.TIMEFORMAT = "%R";
 let base_dir = await $`pwd`;
 base_dir = base_dir.stdout.trim();
@@ -22,15 +22,43 @@ let generations = argv.generation || argv.g || 500;
 let size = argv.size || argv.s || "100x50";
 
 // Build
+console.log(chalk.blue("Building"));
+// C#
 cd("csharp");
 await spinner("Building C# ...", () => $`dotnet build --configuration Release`);
+console.log("    Building C# " + chalk.green("DONE"));
+cd(base_dir);
+
+// Rust
+cd("rust");
+await spinner("Building Rust ...", () => $`cargo build --release`);
+console.log("    Building Rust " + chalk.green("DONE"));
 cd(base_dir);
 
 // Run Benchmark
 const results = new Object();
 for (let i = 0; i < iterations; i++) {
-    console.log(`Running pass ${i + 1} of ${iterations}`);
+    console.log(chalk.blue(`\nPass (${i + 1}/${iterations})`));
 
+    // C#
+    if (i === 0) {
+        results["csharp"] = [];
+    }
+    results["csharp"].push(
+        await time(
+            [
+                "./csharp/bin/Release/net6.0/GameOfLife",
+                "--iterations",
+                generations,
+                "--size",
+                size,
+            ],
+            "Running C# ..."
+        )
+    );
+    console.log("    Running C# " + chalk.green("DONE"));
+
+    // Python
     if (i === 0) {
         results["python"] = [];
     }
@@ -47,24 +75,43 @@ for (let i = 0; i < iterations; i++) {
             "Running Python ..."
         )
     );
+    console.log("    Running Python " + chalk.green("DONE"));
 
+    // Rust
     if (i === 0) {
-        results["csharp"] = [];
+        results["rust"] = [];
     }
-    results["csharp"].push(
+    results["rust"].push(
         await time(
             [
-                "./csharp/bin/Release/net6.0/GameOfLife",
+                "rust/target/release/game_of_life",
                 "--iterations",
                 generations,
                 "--size",
                 size,
             ],
-            "Running C# ..."
+            "Running Rust ..."
         )
     );
+    console.log("    Running Rust " + chalk.green("DONE"));
 }
 
+// Clean
+console.log(chalk.blue("\nCleaning up"));
+// C#
+cd("csharp");
+await spinner("Cleaning up C# ...", () => $`dotnet clean`);
+console.log("    Cleaning up C# " + chalk.green("DONE"));
+cd(base_dir);
+
+// Rust
+cd("rust");
+await spinner("Cleaning up Rust ...", () => $`cargo clean`);
+console.log("    Cleaning up Rust " + chalk.green("DONE"));
+cd(base_dir);
+
+// Print results
+console.log();
 const averages = [["language", "avg time (s)"]];
 for (const [key, value] of Object.entries(results)) {
     let result = average(value);
