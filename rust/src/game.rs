4c41ff0e02084ce1cede0102368f29b6a0960d29
Michael Rivnak <rivnakm1@gmail.com>
2022-11-05 13:55:33 -0400

    working rust version

---
+++ rust/src/game.rs
@@ -0,0 +1,97 @@
+pub fn run_game(height: usize, width: usize, iterations: Option<u32>) {
+    let screen = Screen {
+        height: height,
+        width: width,
+    };
+
+    let mut cells: Vec<bool> = Vec::with_capacity(screen.size_1d());
+    for _ in 0..screen.size_1d() {
+        cells.push(rand::random::<bool>());
+    }
+
+    match iterations {
+        None => {
+            loop {
+                screen.draw(&cells);
+                print!("\x1b[{}A", screen.height);  // Move cursor up
+                next_gen(&mut cells, &screen.height, &screen.width);
+            }
+        }
+        Some(iters) => {
+            for _ in 0..iters {
+                screen.draw(&cells);
+                print!("\x1b[{}A", screen.height);  // Move cursor up
+                next_gen(&mut cells, &screen.height, &screen.width);
+            }
+        }
+    }
+}
+
+fn next_gen(cells: &mut Vec<bool>, height: &usize, width: &usize) {
+    let cells_clone = cells.clone();
+
+    for i in 0..*height {
+        for j in 0..*width {
+            let mut cell = get_cell(&cells_clone, &i, &j, &height, &width);
+            let mut adjacent: u8 = 0;
+
+            for n in 0..3 {
+                for m in 0..3 {
+                    // avoid unsigned int overflow and ignore center cell
+                    if (n == 0 && i == 0) || (m == 0 && j == 0) || (n == 1 && m == 1) {
+                        continue;
+                    }
+                    adjacent +=
+                        get_cell(&cells_clone, &(i + n - 1), &(j + m - 1), &height, &width) as u8;
+                }
+            }
+
+            if cell {
+                if adjacent < 2 {
+                    cell = false;
+                }
+                if adjacent > 3 {
+                    cell = false;
+                }
+            }
+            else {
+                if adjacent == 3 {
+                    cell = true
+                }
+            }
+
+            cells[(i*width)+j] = cell
+        }
+    }
+}
+
+fn get_cell(cells: &Vec<bool>, row: &usize, col: &usize, height: &usize, width: &usize) -> bool {
+    if row < height && col < width {
+        return cells[(*row * *width) + *col];
+    } else {
+        false
+    }
+}
+struct Screen {
+    height: usize,
+    width: usize,
+}
+
+impl Screen {
+    fn draw(&self, cells: &Vec<bool>) {
+        for i in 0..self.height {
+            for j in 0..self.width {
+                if cells[(i * self.width) + j] {
+                    print!("██");
+                } else {
+                    print!("  ");
+                }
+            }
+            println!("");
+        }
+    }
+
+    fn size_1d(&self) -> usize {
+        self.height * self.width
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,39 @@
+use clap::Parser;
+use regex::Regex;
+
+mod game;
+
+/// Conway's Game of Life
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Screen size [WxH]
+    #[arg(short, long)]
+    size: Option<String>,
+
+    /// Number of iterations to run
+    #[arg(short, long)]
+    iterations: Option<u32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let (width, height) = match args.size {
+        None => {
+            let (width, height) = term_size::dimensions().expect("Unable to get terminal size");
+            (width / 2, height - 1)
+        }
+        Some(size) => {
+            let re = Regex::new(r"^(\d+)x(\d+)$").unwrap();
+            let captures = re
+                .captures(size.as_str())
+                .expect(format!("Invalid size '{}'", size).as_str());
+            let width: usize = captures.get(1).unwrap().as_str().parse().unwrap();
+            let height: usize = captures.get(2).unwrap().as_str().parse().unwrap();
+            (width, height)
+        }
+    };
+
+    game::run_game(height, width, args.iterations);
 }
