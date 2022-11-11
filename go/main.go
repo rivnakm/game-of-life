57b7795962fdb353a91ae722bbf67a77e355d137
Michael Rivnak <rivnakm1@gmail.com>
2022-11-11 18:10:40 -0500

    working go version

--- go/main.go
+++ go/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"regexp"
 	"strconv"
+
 	"golang.org/x/term"
 )
 
@@ -14,12 +16,14 @@ func main() {
 	size := flag.String("size", "", "Screen size [WxH]")
 
 	flag.Parse()
-	
+
 	var height int
 	var width int
 
 	if *size == "" {
 		width, height, _ = term.GetSize(0)
+		width = width / 2
+		height = height - 1
 	} else {
 		re, _ := regexp.Compile(`^(\d+)x(\d+)$`)
 
@@ -37,5 +41,88 @@ func main() {
 }
 
 func run_game(height int, width int, generations int) {
-	fmt.Printf("Running %dx%d game with %d generations\n", width, height, generations)
+	cells := make([]bool, height*width)
+	for i := 0; i < height*width; i++ {
+		cells[i] = rand.Float32() > 0.5
+	}
+
+	if generations == -1 {
+		for true {
+			draw_screen(cells, height, width)
+			fmt.Printf("\x1b[%dA", height)
+			next_gen(cells, height, width)
+		}
+	} else {
+		for i := 0; i < generations; i++ {
+			draw_screen(cells, height, width)
+			fmt.Printf("\x1b[%dA", height)
+			next_gen(cells, height, width)
+		}
+	}
+}
+
+func next_gen(cells []bool, height int, width int) {
+	cells_copy := make([]bool, height*width)
+	for i := 0; i < height*width; i++ {
+		cells_copy[i] = cells[i]
+	}
+
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			cell := get_cell(cells_copy, i, j, height, width)
+			adjacent := 0
+
+			for n := -1; n <= 1; n++ {
+				for m := -1; m <= 1; m++ {
+					if (n == -1 && i == 0) || (m == -1 && j == 0) || (n == 0 && m == 0) {
+						continue
+					}
+					adjacent += int(btou(get_cell(cells_copy, i+n, j+m, height, width)))
+				}
+			}
+
+			if cell {
+				if adjacent < 2 {
+					cell = false
+				}
+				if adjacent > 3 {
+					cell = false
+				}
+			} else {
+				if adjacent == 3 {
+					cell = true
+				}
+			}
+
+			cells[i*width+j] = cell
+		}
+	}
+}
+
+func get_cell(cells []bool, row int, col int, height int, width int) bool {
+	if row >= 0 && col >= 0 && row < height && col < width {
+		return cells[(row*width)+col]
+	} else {
+		return false
+	}
+}
+
+func draw_screen(cells []bool, height int, width int) {
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			if cells[(i * width) + j] {
+				fmt.Print("██");
+			} else {
+				fmt.Print("  ");
+			}
+		}
+		fmt.Println("")
+	}
+}
+
+func btou(b bool) uint8 {
+	if b {
+		return 1
+	}
+	return 0
 }
