c6ae8ade2f8fd4d376e922ecd8bdb5cc2437c719
CordlessCoder <>
2023-08-18 16:21:35 +0100

    stupid copypaste NaN iq

--- rust/src/game.rs
+++ rust/src/game.rs
@@ -15,9 +15,9 @@ pub fn run_game<const HEIGHT: usize, const WIDTH: usize, const GENERATIONS: u32>
         // Space for clear
         clear_len +
         // Space for all cells
-        size * 6
+        size * 6 +
         // Space for newlines
-        + HEIGHT
+        HEIGHT
     };
 
     let mut buf = unsafe {
@@ -38,13 +38,14 @@ pub fn run_game<const HEIGHT: usize, const WIDTH: usize, const GENERATIONS: u32>
     // // Clear screen once at the start
     // let _ = stdout.write_all(b"\x1bc");
     for _ in 0..GENERATIONS {
-        screen.draw(&mut buf);
+        // screen.draw(&mut buf);
+        screen.draw_and_next_gen(&mut buf);
         let _ = stdout.write_all(&buf);
         let _ = stdout.flush();
         unsafe {
             buf.set_len(clear_len);
         }
-        screen.next_gen();
+        // screen.next_gen();
     }
 }
 
@@ -81,6 +82,7 @@ impl<const WIDTH: usize, const HEIGHT: usize> Screen<WIDTH, HEIGHT> {
         self.swap.get_unchecked_mut(idx)
     }
 
+    #[allow(dead_code)]
     #[inline(always)]
     fn next_gen(&mut self) {
         #[inline(always)]
@@ -300,11 +302,216 @@ impl<const WIDTH: usize, const HEIGHT: usize> Screen<WIDTH, HEIGHT> {
     }
 
     #[inline(always)]
+    #[allow(dead_code)]
     fn draw(&self, buf: &mut Vec<u8>) {
         for line in self.cells.chunks_exact(WIDTH) {
             unsafe {
                 Self::write_line(buf, line);
             }
         }
+    }
+    #[inline]
+    // Literally just the draw and next_gen functions interleaved into one. It seems to be faster,
+    // somehow!
+    //
+    // DRY evangelists are sobbing rn
+    fn draw_and_next_gen(&mut self, buf: &mut Vec<u8>) {
+        #[inline(always)]
+        fn lives(cell: bool, adjacent: u8) -> bool {
+            (adjacent == 3) | (cell && adjacent == 2)
+        }
+        if WIDTH == 0 || HEIGHT == 0 {
+            // Avoid trying to access non-existant cells
+            return;
+        };
+        if HEIGHT == 1 && WIDTH == 1 {
+            // There is only a single cell, it can't have neighbours
+            self.cells[0] = false;
+            return;
+        };
+        if HEIGHT + WIDTH == 3 {
+            // a 2x1 grid only has 2 cells, thus each can only have 1 neighbour, so all cells must
+            // die
+            self.cells.fill(false);
+            return;
+        }
+        let right = WIDTH - 1;
+        macro_rules! adjacent {
+            ($([$row:expr, $col:expr]),* $(,)?) => {{
+                0 $(
+                    + self.get_cell_unchecked($row, $col) as u8
+                )*
+            }};
+        }
+        macro_rules! cell {
+            (($row:expr, $col:expr)) => {
+                let cell = self.get_cell_unchecked($row, $col);
+                let adjacent = adjacent! {
+                    [$row - 1, $col - 1],
+                    [$row - 1, $col],
+                    [$row - 1, $col + 1],
+                    [$row, $col - 1],
+                    [$row, $col + 1],
+                    [$row + 1, $col - 1],
+                    [$row + 1, $col],
+                    [$row + 1, $col + 1]
+                };
+                *self.get_cell_unchecked_mut($row, $col) = lives(cell, adjacent);
+            };
+            (($row:expr, $col:expr),
+             [
+                $topl:expr, $topm:expr, $topr:expr,
+                $midl:expr, _____, $midr:expr,
+                $botl:expr, $botm:expr, $botr:expr $(,)?
+             ]
+             ) => {
+                let cell = self.get_cell_unchecked($row, $col);
+                let adjacent = if $topl {
+                    self.get_cell_unchecked($row - 1, $col - 1) as u8
+                } else {
+                    0
+                } + if $topm {
+                    self.get_cell_unchecked($row - 1, $col) as u8
+                } else {
+                    0
+                } + if $topr {
+                    self.get_cell_unchecked($row - 1, $col + 1) as u8
+                } else {
+                    0
+                } + if $midl {
+                    self.get_cell_unchecked($row, $col - 1) as u8
+                } else {
+                    0
+                } + if $midr {
+                    self.get_cell_unchecked($row, $col + 1) as u8
+                } else {
+                    0
+                } + if $botl {
+                    self.get_cell_unchecked($row + 1, $col - 1) as u8
+                } else {
+                    0
+                } + if $botm {
+                    self.get_cell_unchecked($row + 1, $col) as u8
+                } else {
+                    0
+                } + if $botr {
+                    self.get_cell_unchecked($row + 1, $col + 1) as u8
+                } else {
+                    0
+                };
+                *self.get_cell_unchecked_mut($row, $col) = lives(cell, adjacent);
+            };
+        }
+        unsafe {
+            // First row
+            Self::write_line(buf, self.swap.get_unchecked(..WIDTH));
+            // Top left cell
+            cell! {
+                (0, 0),
+                [
+                    false, false, false,
+                    false, _____, true,
+                    false, true,  true
+                ]
+            };
+            // Top middle cells
+            // These cells can look for neighbours in all directions but up
+            for col in 1..right {
+                cell! {
+                    (0, col),
+                    [
+                        false, false, false,
+                        true,  _____, true,
+                        true,  true,  true
+                    ]
+                };
+            }
+
+            // Top right cell
+            cell! {
+                (0, right),
+                [
+                    false, false, false,
+                    true,  _____, false,
+                    true,  true,  false
+                ]
+            };
+            // Middle rows
+            for row in 1..HEIGHT - 1 {
+                // First column
+                Self::write_line(
+                    buf,
+                    self.swap
+                        .get_unchecked(WIDTH * row..)
+                        .get_unchecked(..WIDTH),
+                );
+                // can access all neighbours not to the left
+                cell! {
+                    (row, 0),
+                    [
+                        false, true,  true,
+                        false, _____, true,
+                        false, true,  true
+                    ]
+                };
+                // Middle columns
+                // can access all neighbours
+                for col in 1..right {
+                    cell!((row, col));
+                }
+
+                // Last column
+                // can access all neighbours not to the right
+                cell! {
+                    (row, right),
+                    [
+                        true, true,  false,
+                        true, _____, false,
+                        true, true,  false,
+                    ]
+                };
+            }
+            let bottom_row = HEIGHT - 1;
+            // Bottom row
+            Self::write_line(
+                buf,
+                self.swap
+                    .get_unchecked(bottom_row * WIDTH..)
+                    .get_unchecked(..WIDTH),
+            );
+            // Bottom left cell
+            cell! {
+                (bottom_row, 0),
+                [
+                    false, true,  true,
+                    false, _____, true,
+                    false, false, false,
+                ]
+            };
+
+            // Bottom middle cells
+            // These cells can look for neighbours in all directions but down
+            for col in 1..right {
+                cell! {
+                    (bottom_row, col),
+                    [
+                        true, true, true,
+                        true, _____, true,
+                        false, false, false,
+                    ]
+                };
+            }
+
+            // Bottom right cell
+            cell! {
+                (bottom_row, right),
+                [
+                    true,  true,  false,
+                    true,  _____, false,
+                    false, false, false,
+                ]
+            };
+        }
+        std::mem::swap(&mut self.cells, &mut self.swap)
     }
 }
