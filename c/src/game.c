88c7607bcee7914dd812cca28092c0175e9dde62
CordlessCoder <42666308+CordlessCoder@users.noreply.github.com>
2023-08-31 14:52:38 +0100

    Optimized C and Python impletmentations (#18)
    * idx
    
    * optimized Python implementation
    
    * optimized C implementation
    
    * enable static linking for C implementation
    
    ---------
    
    Co-authored-by: CordlessCoder <>

--- c/src/game.c
+++ c/src/game.c
@@ -1,94 +1,189 @@
+#include "lookup.c"
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-#include "game.h"
+#include <unistd.h>
 
 typedef struct {
-    bool* cells;
-    int height;
-    int width;
+    bool  *cells;
+    bool  *swap;
+    size_t height;
+    size_t width;
 } Board;
 
-static bool get_cell(const Board const * board, const int row, const int col) {
-    if (row >= 0 && col >= 0 && row < board->height && col < board->width) {
-        return board->cells[(row * board->width) + col];
-    } else {
-        return false;
-    }
+// static bool get_cell(const Board *board, const size_t row, const size_t col) {
+//     if (row >= 0 && col >= 0 && row < board->height && col < board->width) {
+//         return board->cells[row * board->width + col];
+//     } else {
+//         return false;
+//     }
+// }
+static bool get_cell_unchecked(const Board *board, const size_t row, const size_t col) {
+    return board->cells[row * board->width + col];
 }
 
-static void next_gen(Board* board) {
-    bool* cells_copy = (bool*) malloc(board->height * board->width * sizeof(bool));
-    memcpy(cells_copy, board->cells, (board->height) * (board->width) * sizeof(bool));
-
-    Board board_copy = {cells_copy, board->height, board->width};
-
-    for (int i = 0; i < board->height; i++) {
-        for (int j = 0; j < board->width; j++) {
-            bool cell = get_cell(&board_copy, i, j);
-            int adjacent = 0;
-
-            for (int n = -1; n <= 1; n++) {
-                for (int m = -1; m <= 1; m++) {
-                    if (n == 0 && m == 0) {
-                        continue;
-                    }
-                    if (get_cell(&board_copy, i + n, j + m)) {
-                        adjacent++;
-                    }
-                }
-            }
-
-            if (cell) {
-                if (adjacent < 2) {
-                    cell = false;
-                }
-                if (adjacent > 3) {
-                    cell = false;
-                }
-            } else {
-                if (adjacent == 3) {
-                    cell = true;
-                }
-            }
-
-            board->cells[i * board->width + j] = cell;
+static void next_gen(Board *board) {
+#define LIVES(cell, adjacent) ((adjacent == 3) | (cell && adjacent == 2));
+#define CELL(row, col, tl, tm, tr, ml, mr, bl, bm, br)                    \
+    {                                                                     \
+        bool cell     = get_cell_unchecked(board, row, col);              \
+        int  adjacent = 0;                                                \
+        if (tl) {                                                         \
+            adjacent += get_cell_unchecked(board, row - 1, col - 1);      \
+        };                                                                \
+        if (tm) {                                                         \
+            adjacent += get_cell_unchecked(board, row - 1, col);          \
+        };                                                                \
+        if (tr) {                                                         \
+            adjacent += get_cell_unchecked(board, row - 1, col + 1);      \
+        };                                                                \
+        if (ml) {                                                         \
+            adjacent += get_cell_unchecked(board, row, col - 1);          \
+        };                                                                \
+        if (mr) {                                                         \
+            adjacent += get_cell_unchecked(board, row, col + 1);          \
+        };                                                                \
+        if (bl) {                                                         \
+            adjacent += get_cell_unchecked(board, row + 1, col - 1);      \
+        };                                                                \
+        if (bm) {                                                         \
+            adjacent += get_cell_unchecked(board, row + 1, col);          \
+        };                                                                \
+        if (br) {                                                         \
+            adjacent += get_cell_unchecked(board, row + 1, col + 1);      \
+        };                                                                \
+        board->swap[(board->width) * (row) + col] = LIVES(cell, adjacent) \
+    }                                                                     \
+    // clang-format off
+    // Top row
+    CELL(0ul, 0ul,
+        false, false, false,
+        false,        true,
+        false, true,  true
+    );
+    for (size_t col = 1; col < board->width - 1; col++) {
+        CELL(0ul, col,
+            false, false, false,
+            true,         true,
+            true,  true,  true
+        );
+    }
+    CELL(0ul, board->width - 1,
+        false, false, false,
+        true,         false,
+        true,  true,  false
+    );
+    // Middle row
+    for (size_t row = 1; row < board->height - 1; row++) {
+        CELL(row, 0ul,
+            false, true,  true,
+            false,        true,
+            false, true,  true
+        );
+        for (size_t col = 1; col < board->width - 1; col++) {
+            CELL(row, col,
+            true,  true,  true,
+            true,         true,
+            true,  true,  true
+            )
         }
+        CELL(row, board->width - 1,
+            true,  true,  false,
+            true,         false,
+            true,  true,  false
+        );
+    }
+    // Bottom row
+    CELL(board->height - 1, 0ul,
+        false, true,  true,
+        false,        true,
+        false, false, false
+    );
+    for (size_t col = 1; col < board->width - 1; col++) {
+        CELL(
+            board->height - 1, col,
+            true,  true,  true,
+            true,         true,
+            false, false, false
+        );
     }
 
-    free(cells_copy);
+    // Bottom right cell
+    CELL(
+        board->height - 1, board->width - 1,
+        true,  true,  false,
+        true,         false,
+        false, false, false
+    );
+    // clang-format on
+
+    bool *temp   = board->cells;
+    board->cells = board->swap;
+    board->swap  = temp;
 }
 
-static void draw_screen(const Board const * board) {
-    for (int i = 0; i < board->height; i++) {
-        for (int j = 0; j < board->width; j++) {
+static size_t draw_screen(const Board *board, char *buf) {
+    char *start = buf;
+#define ALIVE     "██"
+#define ALIVE_LEN 6
+#define DEAD      "  "
+#define DEAD_LEN  2
+    for (size_t i = 0; i < board->height; i++) {
+        size_t j = 0;
+        while (j + 8 < board->width) {
+            unsigned char pattern = 0;
+            for (size_t offset = 0; offset < 8; offset++)
+                pattern |= ((unsigned char)board->cells[(i * board->width) + j++]) << offset;
+            length_str out = STRINGS[pattern];
+            memcpy(buf, out.str, out.len);
+            buf += out.len;
+        }
+        for (; j < board->width; j++) {
             if (board->cells[(i * board->width) + j]) {
-                printf("██");
+                memcpy(buf, ALIVE, ALIVE_LEN);
+                buf += ALIVE_LEN;
             } else {
-                printf("  ");
+                memcpy(buf, DEAD, DEAD_LEN);
+                buf += DEAD_LEN;
             }
         }
-        printf("\n");
+        *(buf++) = '\n';
     }
+    return (size_t)(buf - start);
 }
 
-void run_game(int height, int width, int generations) {
-    int size = height * width;
-    bool* cells = (bool*) malloc(size * sizeof(bool));
+void run_game(size_t height, size_t width, int generations) {
+    size_t size  = (size_t)(height * width);
+    bool  *cells = (bool *)malloc(size * sizeof(bool));
+    bool  *swap  = (bool *)malloc(size * sizeof(bool));
+    Board  board = {cells, swap, height, width};
 
-    Board board = {cells, height, width};
+    char clear[64];
+    sprintf(clear, "\x1b[%zuA", height);
+    size_t clearlen = strlen(clear);
+    char  *buf      = (char *)malloc(clearlen
+                                     // Space for all cells
+                                     + size * 6
+                                     // Space for newlines
+                                     + (size_t)height);
+    memcpy(buf, clear, clearlen);
+    char *bufactive = buf + clearlen;
 
     time_t t;
     srand((unsigned)time(&t));
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         board.cells[i] = (rand() % 2 > 0.5);
     }
 
     for (int i = 0; i < generations; i++) {
-        draw_screen(&board);
-        printf("\x1b[%dA", board.height);
+        size_t written = draw_screen(&board, bufactive);
+        fwrite(buf, 1, written + clearlen, stdout);
         next_gen(&board);
     }
+    free(board.cells);
+    free(board.swap);
+    free(buf);
 }
--- python/board.py
+++ python/board.py
@@ -1,27 +1,30 @@
 import random
 from typing import List
+from io import StringIO
+
+# import copy
+
 
 class Board:
-    def __init__(self, height: int, width: int, cells: List[bool] = []):
+    def __init__(self, height: int, width: int, cells: List[bool] = [], swap: List[bool] = []):
         self.cells: List[bool] = [bool(random.getrandbits(1)) for _ in range(height * width)] if len(cells) == 0 else cells
+        self.swap: List[bool] = [False] * (height * width) if len(swap) == 0 else swap
         self.height = height
         self.width = width
 
-    def copy(self) -> "Board":
-        return Board(self.height, self.width, self.cells.copy())
-
-    def draw(self):
+    def draw(self, buf: StringIO) -> StringIO:
         for i in range(self.height):
             for j in range(self.width):
-                if self.cells[(i*self.width)+j]:
-                    print("██", end="")
+                if self.cells[(i * self.width) + j]:
+                    buf.write("██")
                 else:
-                    print("  ", end="")
-            print("")
+                    buf.write("  ")
+            buf.write("\n")
+        return buf
 
-    def get_cell(self, row: int, col: int) -> bool:
+    def get_cell(self, row: int, col: int) -> int:
         if 0 <= row < self.height and 0 <= col < self.width:
-            return self.cells[(row*self.width)+col]
+            return self.cells[(row * self.width) + col]
         else:
             return False
 
